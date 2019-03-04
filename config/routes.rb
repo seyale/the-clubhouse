469ca1fd55e3b20a9dc6c40fc5620c1d66de306c
Christian Lopes <cfmlopes@hotmail.co.uk>
2019-03-04 16:54:48 +0000

    routes completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
-  get 'games/new'
-  get 'games/create'
-  get 'games/index'
-  get 'games/edit'
-  get 'games/update'
-  get 'games/destroy'
-
-  resources :games, except: [:show]
   devise_for :users
+  resources :users
+  resources :games, except: [:show]
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
