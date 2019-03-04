c48fc6daa2bd1bcd940d6fa8ed2f84888666db1b
Alexander Seyss <a@seyss.at>
2019-03-04 15:06:32 +0000

    minor change

--- db/migrate/20190304141111_create_games.rb
+++ db/migrate/20190304141111_create_games.rb
@@ -2,7 +2,7 @@ class CreateGames < ActiveRecord::Migration[5.2]
   def change
     create_table :games do |t|
       t.references :user, foreign_key: true
-      t.integer :skill_level
+      t.string :skill_level
       t.boolean :game_type, default: true
       t.date :date
       t.time :time
