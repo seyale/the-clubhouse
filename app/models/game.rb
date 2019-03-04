d4e9a46621069eb929241ff80970030261b05acb
Christian Lopes <cfmlopes@hotmail.co.uk>
2019-03-04 14:41:39 +0000

    PUSH TO MASTER

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,9 @@
 class Game < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, dependent: :destroy
+  has_many :users_games
+  has_many :users, through: :users_games
+
+  validates :date, presence: true
+  validates :time, presence: true
+  validates :skill_level, presence: true
 end
