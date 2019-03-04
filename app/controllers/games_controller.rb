d4be11a3f8021ee3009951db6687e44303232400
Kevin Willson <willson.kevin@gmail.com>
2019-03-04 18:02:33 +0000

    began games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,11 +1,21 @@
 class GamesController < ApplicationController
+  before_action :set_game, only: [:edit, :update, :destroy]
+
   def new
+    @game = Game.new
   end
 
   def create
+    @game = Game.new(game_params)
+    if @game.save
+      redirect_to game_path(@game)
+    else
+      render :new
+    end
   end
 
   def index
+    @games = Game.all
   end
 
   def edit
@@ -15,5 +25,16 @@ class GamesController < ApplicationController
   end
 
   def destroy
+    @game = Game.destroy
+  end
+
+  private
+
+  def set_game
+    @game = Game.find(params[:id])
+  end
+
+  def game_params
+    params.require(:game).permit(:user_id, :skill_level, :game_type, :date, :time, :additional_info)
   end
 end
