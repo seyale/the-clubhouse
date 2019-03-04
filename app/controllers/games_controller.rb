fa8709ab3931ec23ca95b70af279c36ecce54375
Christian Lopes <cfmlopes@hotmail.co.uk>
2019-03-04 16:50:05 +0000

    games controller created

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,19 @@
+class GamesController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'games/new'
+  get 'games/create'
+  get 'games/index'
+  get 'games/edit'
+  get 'games/update'
+  get 'games/destroy'
+
+  resources :games, except: [:show]
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
