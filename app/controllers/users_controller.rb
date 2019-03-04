b3aa75469aff7e056a3af71126e534efb0a08091
Christian Lopes <cfmlopes@hotmail.co.uk>
2019-03-04 17:44:28 +0000

    Users controller completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
   def show
   end
 
@@ -6,8 +8,25 @@ class UsersController < ApplicationController
   end
 
   def update
+    if @user.update(user_params)
+      redirect_to user_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @user.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :description, :skill_level)
   end
 end
