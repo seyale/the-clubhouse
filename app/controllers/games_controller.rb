287013d71f4f32909bb4032e1413a0b7e295495b
Kevin Willson <willson.kevin@gmail.com>
2019-03-05 11:57:45 +0000

    modified games controller, new game view form, and games index

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,8 +7,9 @@ class GamesController < ApplicationController
 
   def create
     @game = Game.new(game_params)
+    @game.user = current_user
     if @game.save
-      redirect_to game_path(@game)
+      redirect_to games_path
     else
       render :new
     end
