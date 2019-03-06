f36b2b73ca3d649f794b0796effe2caacca54d53
Kevin Willson <willson.kevin@gmail.com>
2019-03-06 14:08:01 +0000

    changed routes to match

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'join_game', to: 'pages#join_game'
   get 'events', to: 'pages#events'
-  get 'users_games/new'
-  get 'users_games/create'
+  get 'participations/new'
+  get 'participations/create'
 end
