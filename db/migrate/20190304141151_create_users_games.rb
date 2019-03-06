b9386edd67439c67df969507b1fba217fa9cc577
Christian Lopes <cfmlopes@hotmail.co.uk>
2019-03-06 14:08:16 +0000

    please work, i changed the name of the table to be participations and the user_id is is now called host_id in the games table. Kevin has the other half of this mess we made. Oh god dont make me regret this please. TOUCH WOOD

--- db/migrate/20190304141151_create_users_games.rb
+++ db/migrate/20190304141151_create_users_games.rb
@@ -1,6 +1,6 @@
-class CreateUsersGames < ActiveRecord::Migration[5.2]
+class CreateParticipations < ActiveRecord::Migration[5.2]
   def change
-    create_table :users_games do |t|
+    create_table :participations do |t|
       t.references :user, foreign_key: true
       t.references :game, foreign_key: true
 
