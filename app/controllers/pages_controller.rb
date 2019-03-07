8b01ce89aa609d136ecea268f5ad63334f596e45
Christian Lopes <cfmlopes@hotmail.co.uk>
2019-03-07 10:59:38 +0000

    join_game cotroller method done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,13 +2,20 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    # if current_user.nil?
+    #   redirect_to (this is for you kevin)
+
     # @join = Game.where(status: "pending")
     # @watch = Game.where(status: "complete")
     # @my_games =
   end
 
   def join_game
-    @games = Game.where(status: "pending")
+    @all_games = Game.where(status: "pending") #, host: :current_user) #  && user_id: current_user.id
+    @games = []
+    @all_games.each do |game|
+      @games << game if (game.host_id != current_user.id && !game.users.include?(current_user))
+    end
   end
 
   def events
