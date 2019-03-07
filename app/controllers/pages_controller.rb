04a340717ac799c09f21124bce819766786d5049
Kevin Willson <willson.kevin@gmail.com>
2019-03-07 14:18:48 +0000

    fixed mistake

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
-  before_action :disable_nav, only: [:landing]
+
 
   def home
     # if current_user.nil?
