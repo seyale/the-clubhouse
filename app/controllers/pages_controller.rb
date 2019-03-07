6670a73f667af5e83be23a59d318b806402d7af6
Kevin Willson <willson.kevin@gmail.com>
2019-03-07 14:14:33 +0000

    added landing method to Pages controller, altered application.html.erb to make sure NavBar wouldn't load on landing page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
+  before_action :disable_nav, only: [:landing]
 
   def home
     # if current_user.nil?
@@ -19,5 +20,8 @@ class PagesController < ApplicationController
   end
 
   def events
+  end
+
+  def landing
   end
 end
