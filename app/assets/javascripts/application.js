cdaeba6724be4a680482c35a2105b63b998246ee
Alexander Seyss <a@seyss.at>
2019-03-08 14:38:54 +0000

    court outline png added to assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,7 +16,7 @@ function openGame(evt, gameName) {
   // Get all elements with class="tablinks" and remove the class "active"
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
+    tablinks[i].className = tablinks[i].className.replace("active", "");
   }
 
   // Show the current tab, and add an "active" class to the button that opened the tab
